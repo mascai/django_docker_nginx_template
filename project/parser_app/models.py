cac4486e3d86230bddf37380af83d8e3440edb2d
mascai <msumoskalenko@gmail.com>
2022-04-23 23:21:59 +0300

    Improve docker env and parsing logic

--- project/parser_app/models.py
+++ project/parser_app/models.py
@@ -3,11 +3,21 @@ from django.db import models
 
 class BaseTask(models.Model):
     name = models.CharField(max_length=100)
-    is_active = models.BooleanField(default=True) # True - task is running
-    data = models.TextField(blank=True)
+    is_success = models.BooleanField(default=False) # True - task is running
     
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
+
+
+class BaseParsingResult(models.Model):
+    task_id = models.ForeignKey(
+        BaseTask,
+        blank=True,
+        null=True,
+        on_delete=models.PROTECT
+    )
+    data = models.TextField(blank=True)
+    task_type = models.CharField(blank=True, max_length=64)
